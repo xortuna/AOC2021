34717c030ec5bf116f26f8e8415c7dd8847682d0
J D <jackdye201@hotmail.com>
2021-12-02 13:45:18 +0000

    Completed day 2

--- Day02/Day2/Program.cs
+++ Day02/Day2/Program.cs
@@ -12,49 +12,68 @@ namespace Day2
         {
             Console.WriteLine("Hello World!");
             //Part 1
+            int HorizontalPosition = 0;
+            int Depth = 0;
+            string line;
+            Regex lineSplit = new Regex( @"(\w+)\s(\d)");
             using (StreamReader sr = new StreamReader("puzzleinput.txt"))
             {
-                string line = null;
-                int increase = 0;
-                int? cache = null;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    var l = int.Parse(line);
-
-                    if (cache.HasValue && cache.Value < l)
-                        increase++;
-                    cache = l;
-                }
-
-                Console.WriteLine($"Part 1: Increases {increase}");
-                Console.ReadLine();
-            }
-            //Part 2
-            using (StreamReader sr = new StreamReader("puzzleinput.txt"))
-            {
-                string line = null;
-                int increase = 0;
-                int? cache = null;
-                Queue<int> rollingWindow = new Queue<int>();
-                while ((line = sr.ReadLine()) != null)
-                {
-                    var l = int.Parse(line);
-                    rollingWindow.Enqueue(l);
-                    if (rollingWindow.Count >= 3)
+                    var s = lineSplit.Match(line);
+                    if(s.Success)
                     {
-                        if(rollingWindow.Count > 3)
-                            rollingWindow.Dequeue();
-                        var sum = rollingWindow.Sum();
-                        if (cache.HasValue && cache.Value < sum)
-                            increase++;
-                        cache = sum;
+                        int distance = int.Parse(s.Groups[2].Value);
+                        switch (s.Groups[1].Value) //direction
+                        {
+                            case "forward":
+                                HorizontalPosition += distance;
+                                break;
+                            case "down":
+                                Depth += distance;
+                                break;
+                            case "up":
+                                Depth -= distance;
+                                break;
+
+                        }
+
                     }
                 }
 
-                Console.WriteLine($"Part 2: Increases {increase}");
-                Console.ReadLine();
+                Console.WriteLine($"F:{HorizontalPosition} D:{Depth} Part 1:{HorizontalPosition * Depth}");
             }
+            int Aim = 0;
+            Depth = HorizontalPosition = 0;
+            using (StreamReader sr = new StreamReader("puzzleinput.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var s = lineSplit.Match(line);
+                    if (s.Success)
+                    {
+                        int distance = int.Parse(s.Groups[2].Value);
+                        switch (s.Groups[1].Value) //direction
+                        {
+                            case "forward":
+                                HorizontalPosition += distance;
+                                Depth += Aim * distance;
+                                break;
+                            case "down":
+                                Aim += distance;
+                                break;
+                            case "up":
+                                Aim -= distance;
+                                break;
 
+                        }
+
+                    }
+                }
+
+                Console.WriteLine($"F:{HorizontalPosition} D:{Depth} Part 2:{HorizontalPosition * Depth}");
+            }
+            Console.ReadLine();
         }
     }
 }

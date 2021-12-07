07fc4e6e2e21c2bb85e42ead2c193705c1f7961a
J D <jackdye201@hotmail.com>
2021-12-07 17:07:42 +0000

    Added day 4

--- Day03/Day3/Program.cs
+++ Day03/Day3/Program.cs
@@ -8,130 +8,88 @@ namespace Day2
 {
     class Program
     {
-        enum ReadState {
-            BingoBalls,
-            WaitForCard,
-            ProcessCard,
-        }
-
-        class BingoNumber
-        {
-            public int Number;
-            public bool Seen = false;
-
-            public BingoNumber(int n)
-            {
-                Number = n;
-            }
-        }
-
-        class BingoCard
-        {
-            public List<List<BingoNumber>> GridState = new List<List<BingoNumber>>();
-
-            public bool Mark(int number)
-            {
-                foreach(var row in GridState)
-                {
-                    foreach(var col in row)
-                    {
-                        if (col.Number == number) 
-                            col.Seen = true;
-                    }
-                }
-                return HasBingo();
-            }
-
-            public bool HasBingo()
-            {
-                //row bingo
-                foreach(var row in GridState)
-                {
-                    if (row.All(t => t.Seen))
-                        return true;
-                }
-
-                //Column bingo
-                for(int i =0; i < GridState[0].Count; ++i)
-                {
-                    if (GridState.All(t => t[i].Seen))
-                        return true;
-                }
-                return false;
-            }
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            List<int> bingoNumbers = new List<int>();
-            List<BingoCard> cards = new List<BingoCard>();
-            ReadState state = ReadState.BingoBalls;
-            
+            int[] bits = null;
             //Part 1
-            using (StreamReader sr = new StreamReader("puzzleinput.txt"))
+            BitCount(File.ReadLines("puzzleinput.txt"), out bits);
+            long gamma = 0;
+            long epsilong = 0;
+            for (int i = 0; i < bits.Length; ++i)
             {
-                string line = null;
-                while ((line = sr.ReadLine()) != null)
+                if (bits[i] > 0)
                 {
-
-                    switch (state)
-                    {
-                        case ReadState.BingoBalls:
-                            bingoNumbers = line.Split(",").Select(t=>int.Parse(t)).ToList();
-                            state = ReadState.WaitForCard;
-                            break;
-                        case ReadState.WaitForCard:
-                            if (string.IsNullOrEmpty(line))
-                                continue;
-
-                            cards.Add(new BingoCard());
-                            cards.Last().GridState.Add(line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(t => new BingoNumber(int.Parse(t))).ToList());
-                            state = ReadState.ProcessCard;
-                            break;
-                        case ReadState.ProcessCard:
-                            if (string.IsNullOrEmpty(line))
-                            {
-                                state = ReadState.WaitForCard;
-                                continue;
-                            }
-
-                            cards.Last().GridState.Add(line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(t => new BingoNumber(int.Parse(t))).ToList());
-                            break;
-                    }
+                    gamma = gamma << 1 | 1;
+                    epsilong = epsilong << 1;
                 }
-
-            }
-            bool donePart1 = false;
-            foreach(var ball in bingoNumbers)
-            {
-                foreach(var card in cards)
+                else
                 {
-                    if (card.Mark(ball))
-                    {
-                        if (!donePart1)
-                        {
-                            OutputPuzzleAnswer("Part 1", card, ball);
-                            donePart1 = true;
-                        }
-                        if(cards.All(t => t.HasBingo()))
-                        {
-                            OutputPuzzleAnswer("Part 2", card, ball);
-                            goto exit;
-                        }
-
-                    }
-                }            
+                    gamma = gamma << 1;
+                    epsilong = epsilong << 1 | 1;
+                }
             }
-            exit:
+
+            Console.WriteLine($"Part 1: {gamma*epsilong}");
+            //Part 2
+            List<string> o2Lines = File.ReadLines("puzzleinput.txt").ToList();
+            List<string> co2Lines = File.ReadLines("puzzleinput.txt").ToList();
+            int pos = 0;
+            do
+            {
+                
+                int c = 0;
+                foreach (var line in o2Lines)
+                {
+                    if (line[pos] == '0')
+                        c--;
+                    else
+                        c++;
+                }
+                o2Lines.RemoveAll(t => t[pos] == ((c < 0) ? '1' : '0'));
+                pos++;
+            }
+            while (o2Lines.Count > 1);
+
+            pos = 0;
+            do
+            {
+                int c = 0;
+                foreach (var line in co2Lines)
+                {
+                    if (line[pos] == '0')
+                        c--;
+                    else
+                        c++;
+                }
+                co2Lines.RemoveAll(t => t[pos] == ((c < 0) ? '0' : '1'));
+                pos++;
+            }
+            while (co2Lines.Count > 1);
+
+            Console.WriteLine($"Part 2: {Convert.ToUInt64(o2Lines.First(), 2) * Convert.ToUInt64(co2Lines.First(), 2)}");
             Console.ReadLine();
+
         }
 
-        private static void OutputPuzzleAnswer(string title, BingoCard card, int ball)
-        {
-            var totalUnseen = card.GridState.Sum(t => t.Where(r => !r.Seen).Sum(r => r.Number));
 
-            Console.WriteLine($"{title} {totalUnseen} * {ball} = {totalUnseen*ball}");
+        static void BitCount(IEnumerable<string> lines, out int[] bits)
+        {
+            bits = null;
+            foreach (var line in lines)
+            {
+                if (bits == null)
+                {
+                    bits = new int[line.Length];
+                }
+                for (int i = 0; i < line.Length; ++i)
+                {
+                    if (line[i] == '0')
+                        bits[i]--;
+                    else
+                        bits[i]++;
+                }
+            }
         }
     }
 }
---
+++ Day04/Day3/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day2
+{
+    class Program
+    {
+        enum ReadState {
+            BingoBalls,
+            WaitForCard,
+            ProcessCard,
+        }
+
+        class BingoNumber
+        {
+            public int Number;
+            public bool Seen = false;
+
+            public BingoNumber(int n)
+            {
+                Number = n;
+            }
+        }
+
+        class BingoCard
+        {
+            public List<List<BingoNumber>> GridState = new List<List<BingoNumber>>();
+
+            public bool Mark(int number)
+            {
+                foreach(var row in GridState)
+                {
+                    foreach(var col in row)
+                    {
+                        if (col.Number == number) 
+                            col.Seen = true;
+                    }
+                }
+                return HasBingo();
+            }
+
+            public bool HasBingo()
+            {
+                //row bingo
+                foreach(var row in GridState)
+                {
+                    if (row.All(t => t.Seen))
+                        return true;
+                }
+
+                //Column bingo
+                for(int i =0; i < GridState[0].Count; ++i)
+                {
+                    if (GridState.All(t => t[i].Seen))
+                        return true;
+                }
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<int> bingoNumbers = new List<int>();
+            List<BingoCard> cards = new List<BingoCard>();
+            ReadState state = ReadState.BingoBalls;
+            
+            //Part 1
+            using (StreamReader sr = new StreamReader("puzzleinput.txt"))
+            {
+                string line = null;
+                while ((line = sr.ReadLine()) != null)
+                {
+
+                    switch (state)
+                    {
+                        case ReadState.BingoBalls:
+                            bingoNumbers = line.Split(",").Select(t=>int.Parse(t)).ToList();
+                            state = ReadState.WaitForCard;
+                            break;
+                        case ReadState.WaitForCard:
+                            if (string.IsNullOrEmpty(line))
+                                continue;
+
+                            cards.Add(new BingoCard());
+                            cards.Last().GridState.Add(line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(t => new BingoNumber(int.Parse(t))).ToList());
+                            state = ReadState.ProcessCard;
+                            break;
+                        case ReadState.ProcessCard:
+                            if (string.IsNullOrEmpty(line))
+                            {
+                                state = ReadState.WaitForCard;
+                                continue;
+                            }
+
+                            cards.Last().GridState.Add(line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(t => new BingoNumber(int.Parse(t))).ToList());
+                            break;
+                    }
+                }
+
+            }
+            bool donePart1 = false;
+            foreach(var ball in bingoNumbers)
+            {
+                foreach(var card in cards)
+                {
+                    if (card.Mark(ball))
+                    {
+                        if (!donePart1)
+                        {
+                            OutputPuzzleAnswer("Part 1", card, ball);
+                            donePart1 = true;
+                        }
+                        if(cards.All(t => t.HasBingo()))
+                        {
+                            OutputPuzzleAnswer("Part 2", card, ball);
+                            goto exit;
+                        }
+
+                    }
+                }            
+            }
+            exit:
+            Console.ReadLine();
+        }
+
+        private static void OutputPuzzleAnswer(string title, BingoCard card, int ball)
+        {
+            var totalUnseen = card.GridState.Sum(t => t.Where(r => !r.Seen).Sum(r => r.Number));
+
+            Console.WriteLine($"{title} {totalUnseen} * {ball} = {totalUnseen*ball}");
+        }
+    }
+}
